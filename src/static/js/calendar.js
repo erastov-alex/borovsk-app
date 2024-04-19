aece5f8720fac3b207a3d0b8ffd8b85148f7ea67
erastov-alex <erastov-alex@gmail.com>
2024-04-19 18:37:16 +0300

    add calendar

--- src/static/js/calendar.js
+++ src/static/js/calendar.js
@@ -3,21 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Обработчик события для кнопки "Выбрать"
     selectDateButton.addEventListener('click', function () {
-        const startDate = document.getElementById('start_date').value;
-        const endDate = document.getElementById('end_date').value;
+        // Получаем значение поля ввода с идентификатором 'airdatepicker'
+        const calendarInput = document.getElementById('airdatepicker').value;
+        
+        // Разделяем строку на две части по ', '
+        const dates = calendarInput.split(', ');
 
-        // Проверяем, чтобы выбраны были обе даты
-        if (startDate && endDate) {
-            // Проверяем, чтобы конечная дата была позже начальной даты
-            if (startDate <= endDate) {
-                const houseId = document.getElementById('house_id').value; // Получаем house_id из скрытого поля
-                // Переходим на страницу booking_confirmation.html с передачей параметров
-                window.location.href = `/booking_confirmation?house_id=${houseId}&start_date=${startDate}&end_date=${endDate}`;
-            } else {
-                alert('Конечная дата должна быть позже начальной даты');
-            }
-        } else {
-            alert('Пожалуйста, выберите начальную и конечную даты');
-        }
+        // Получаем отдельные части массива
+        const startDate = dates[0];
+        const endDate = dates[1];
+        
+        // Получаем значение поля ввода с идентификатором 'house_id'
+        const houseId = document.getElementById('house_id').value;
+
+        // Переходим на страницу booking_confirmation.html с передачей параметров
+        window.location.href = `/booking_confirmation?house_id=${houseId}&start_date=${startDate}&end_date=${endDate}`;
     });
 });
