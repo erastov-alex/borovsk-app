a683654c27042fa95fa2f3afdcea15db2d565d9f
erastov-alex <erastov-alex@gmail.com>
2024-06-01 17:33:36 +0300

    reg ru application rule

--- app.py
+++ app.py
@@ -8,51 +8,51 @@ from src.api.routes import api
 
 from src.utils.cache import init_cache
 
-app = Flask(__name__, template_folder="src/templates", static_folder="src/static")
+application = Flask(__name__, template_folder="src/templates", static_folder="src/static")
 
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+application.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
+application.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
 
-init_cache(app)
-app.config["CACHE_TYPE"] = CACHE_TYPE
+init_cache(application)
+application.config["CACHE_TYPE"] = CACHE_TYPE
 
 from flask_wtf import CSRFProtect
 
-csrf = CSRFProtect(app)
+csrf = CSRFProtect(application)
 
 # Инициализация базы данных
-db.init_app(app)
+db.init_app(application)
 
 from src.models.users import User
 from src.models.bookings import Booking
 from src.models.houses import House
 
-migrate = Migrate(app, db)
+migrate = Migrate(application, db)
 
-jwt = Api(app, os.getenv("JWT_SECRET_KEY"))
+jwt = Api(application, os.getenv("JWT_SECRET_KEY"))
 
 from src.login import login_manager
 
-login_manager.init_app(app)
+login_manager.init_app(application)
 
 from src.routes.main_routes import main_bp
 from src.routes.users_routes import users_bp
 from src.routes.booking_routes import bookings_bp
 from src.routes.admin_routes import admin_bp
 
-app.register_blueprint(main_bp)
-app.register_blueprint(users_bp)
-app.register_blueprint(bookings_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(api)
+application.register_blueprint(main_bp)
+application.register_blueprint(users_bp)
+application.register_blueprint(bookings_bp)
+application.register_blueprint(admin_bp)
+application.register_blueprint(api)
 
-app.config['WTF_CSRF_ENABLED'] = False 
+application.config['WTF_CSRF_ENABLED'] = False 
 
 # Создание таблиц в базе данных
 # with app.app_context():
 #     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
